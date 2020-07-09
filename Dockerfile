e7573cab4508306b162b86404e79af2540df555d
Kothamasu <vasudeva@softwareag.com>
2020-07-09 12:13:00 +0530

    First commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+# Step to build phase
+FROM node:alpine as builder
+WORKDIR '/app'
+COPY package.json .
+RUN npm install
+COPY . .
+RUN npm run build
+
+# Run phase for NGINX 
+
+FROM nginx
+COPY --from=builder /app/build /usr/share/nginx/html
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          yes   Edit <code>src/App.js</code> and save to reload is modified again and again and on 7/9/2020 july 9th.
+		  with docker compose again
+		  whatever
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
